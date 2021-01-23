975b564e0c8c6e6f62a37400fa7d5b4b0a0c50e0
Katie Aquino <katiemaryfrances.aquino@gmail.com>
2021-01-23 00:15:10 +0000

    Create Customer Object and Customer Info Dictionary

--- customers.py
+++ customers.py
@@ -4,4 +4,49 @@
 class Customer(object):
     """Ubermelon customer."""
 
-    # TODO: need to implement this
+    def __init__(self,
+                 first_name,
+                 last_name,
+                 email,
+                 password):
+        
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+    
+    def __repr__(self):
+        """Convenience method to show info about customer"""
+
+        return f"<Customer: {self.first_name} {self.last_name}, email: {email}"
+
+def read_customers_from_file(filepath):
+    """Read customer data from file & populate dictionary of customers
+
+    Dictionary will be {email: customer()}
+    """
+
+    customer_info = {}
+
+    with open(filepath) as file:
+        for line in file:
+            (first_name,
+            last_name,
+            email,
+            password) = line.strip().split('|')
+
+            customer_info[email] = Customer(first_name,
+                                            last_name,
+                                            email,
+                                            password)
+    
+    return customer_info
+
+def get_by_email(email):
+    """Returns a customer given an email"""
+    
+    return customer_info[email]
+
+
+
+customer_info = read_customers_from_file("customers.txt")
